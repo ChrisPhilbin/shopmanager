95e5d4fcba23b2f95f2f7049590c06fa55533dd6
Chris Philbin <chris@chrisphilbin.net>
2019-06-03 08:00:58 -0400

    Adding additional functionality for edit customer form and customer controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -12,4 +12,19 @@ class CustomersController < ApplicationController
 		@customer = Customer.find(params[:id])
 	end
 
+	def update
+		@customer = Customer.find(params[:id])
+		if @customer.update_attributes(customer_params)
+
+		else
+			render "edit"
+		end
+	end
+
+	private
+
+	def customer_params
+		params.require(:customer).permit(:firstname, :lastname, :email, :phone)
+	end
+
 end
