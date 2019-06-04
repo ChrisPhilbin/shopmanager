506e70487c3c1388021006bb9f3c5a6b46b6ba66
Chris Philbin <chris@chrisphilbin.net>
2019-06-03 21:06:39 -0400

    Moving devise controllers to customize them

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'static#index'
 
-  devise_for :customers
+  devise_for :customers, controllers: { sessions: 'customers/sessions'}
   resources :teches
   resources :repairs
   resources :customers
