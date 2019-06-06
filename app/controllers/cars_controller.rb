9017642c7028f0bac84be9a0b4696447ebcd4d0d
Chris Philbin <chris@chrisphilbin.net>
2019-06-06 19:21:09 -0400

    Fixing nested route by passing in current_user.id to link_to

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -8,4 +8,17 @@ class CarsController < ApplicationController
 		@car = Car.new
 	end
 
+	def create
+		@car = Car.new
+		if car.save?
+			redirect_to customer_cars_path
+		else
+			render 'new'
+		end
+	end
+
+	def index
+		@cars = current_customer.cars
+	end
+
 end
