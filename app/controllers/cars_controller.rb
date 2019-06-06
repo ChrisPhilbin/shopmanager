fd98b5558bc8b65ff4b89244f7c1bd401202b61c
Chris Philbin <chris@chrisphilbin.net>
2019-06-06 19:42:50 -0400

    Adding nested form to allow customer to create and assign a new car object back to themselves

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -5,12 +5,12 @@ class CarsController < ApplicationController
 	end
 
 	def new
-		@car = Car.new
+		@car = Car.new(customer_id: current_customer.id)
 	end
 
 	def create
-		@car = Car.new
-		if car.save?
+		@car = Car.new(car_params)
+		if @car.save?
 			redirect_to customer_cars_path
 		else
 			render 'new'
@@ -19,6 +19,12 @@ class CarsController < ApplicationController
 
 	def index
 		@cars = current_customer.cars
+	end
+
+	private
+
+	def car_params
+		params.require(:car).permit(:year, :make, :model, :customer_id)
 	end
 
 end
