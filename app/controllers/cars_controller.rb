83c6698617f70459a8d9552d7dcbb625a532da9d
Chris Philbin <chris@chrisphilbin.net>
2019-06-06 20:13:20 -0400

    Adding update action to cars_controller to handle updated params from edit form

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -22,7 +22,13 @@ class CarsController < ApplicationController
 	end
 
 	def edit
+		@car = Car.find(params[:id])
+	end
 
+	def update
+		@car = Car.find(params[:id])
+		@car.update_attributes(car_params)
+		redirect_to customer_cars_path
 	end
 
 	private
