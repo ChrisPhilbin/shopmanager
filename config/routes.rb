6eca6e152d5f3cd5ec0b5dccda215e1a87bf2699
Chris Philbin <chris@chrisphilbin.net>
2019-06-06 22:26:45 -0400

    Adding nested routes for repairs below cars

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   resources :teches
   resources :repairs
   resources :customers do
-  	resources :cars
-  	# resources :repairs
+  	resources :cars do
+  		resources :repairs
+  	end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
