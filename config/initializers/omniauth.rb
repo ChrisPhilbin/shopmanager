4cac62c8d86bd5728ede172e2f4d64451b578721
Chris Philbin <31868205+ChrisPhilbin@users.noreply.github.com>
2019-06-10 07:48:56 -0400

    Delete omniauth.rb

--- config/initializers/omniauth.rb
+++
@@ -1,3 +0,0 @@
-    Rails.application.config.middleware.use OmniAuth::Builder do
-      provider :facebook, "452157338664385", "0412c96fee8bd0ccbeaff0bf64275266", {:provider_ignores_state => true}
-    end
